d3b1afc40cfdffc7dd19679786de5b3f0a3c743d
serge <serge.vojeykov@yandex.ru>
2022-09-06 18:35:52 +0300

    Exit button added

--- terracon/terracon.py
+++ terracon/terracon.py
@@ -4,13 +4,24 @@ import sys
 from PyQt5 import QtWidgets
 
 
+class TerraconWindow(QtWidgets.QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle('TerraCon')
+		self.mainLayout = QtWidgets.QVBoxLayout()
+		self.setLayout(self.mainLayout)
+		self.exitButton = QtWidgets.QPushButton('Exit')
+		self.mainLayout.addWidget(self.exitButton)
+		self.exitButton.clicked.connect(QtWidgets.QApplication.quit)
+
+
 def main():
 	app = QtWidgets.QApplication(sys.argv)
 
-	mainwnd = QtWidgets.QWidget()
+	mainwnd = TerraconWindow()
 	#w.resize(250, 150)
 	#w.move(300, 300)
-	mainwnd.setWindowTitle('TerraCon')
+	#mainwnd.setWindowTitle('TerraCon')
 	mainwnd.show()
 
 	sys.exit(app.exec())
