f3a0ade0f2a765dd494c0a2c8c70e37c17d28926
Serge Gron <serge.gron@gmail.com>
2023-06-28 17:10:16 +0300

    TerraconProgramEngine moved to main module. Added link between Task and Worker.

--- terracon/programs/program_test1.py
+++ terracon/programs/program_test1.py
@@ -1,4 +1,4 @@
-from terracon_program_engine import *
+from terracon import TerraconProgramEngine, Task
 from datetime import time
 
 class RootTask(Task):
@@ -8,11 +8,14 @@ class RootTask(Task):
         self.min_light_intensity = 1
         self.max_light_intensity = 99
 
-    def step(self, engine: TerraconProgramEngine):
+    def step(self, engine):
         # если в списке заданий не найден LightingManager, создаем его
         lm = engine.find_task("LightingManager")
         if not lm:
             engine.new_task(LightingManager)
+
+    def apply(self, engine):
+        engine.worker.main_light_intensity = self.light_intensity
 
 
 class LightingManager(Task):
@@ -36,7 +39,7 @@ class Sunrise(Task):
         self.duration = 5  # в секундах
         self.k = 0
 
-    def step(self, engine: TerraconProgramEngine):
+    def step(self, engine):
         if self.k == 0:
             self.k = (self.root.max_light_intensity - self.root.min_light_intensity) / self.duration
 
@@ -49,13 +52,15 @@ class Sunrise(Task):
         if self.root.light_intensity >= self.root.max_light_intensity:
             print("Sunrise: light at max, finishing")
             self.finish()
+        self.root.apply(engine)
 
 class Sunset(Task):
     def __init__(self, name):
         super().__init__(name)
 
-    def step(self, engine: TerraconProgramEngine):
+    def step(self, engine):
         self.root.light_intensity -= 1
         if self.root.light_intensity <= self.min_light_intensity:
             print("Sunset: light at min, finishing")
             self.finish()
+        self.root.apply(engine)
--- terracon/terracon.py
+++ terracon/terracon.py
@@ -6,9 +6,12 @@ import asyncio
 import websockets
 import xml.etree.ElementTree as etree
 import xml.dom.minidom as md
-import terracon_program_engine as progengine
 import json
 from functools import partial
+from datetime import datetime
+import importlib
+import importlib.util
+
 
 gpio_present = True
 
@@ -47,6 +50,79 @@ class WebServer:
         await self.client.send(message)
 
 
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.root = None
+        self.is_done = False
+        self.start_time_stamp = datetime.now()
+
+    def time_from_start_sec(self):
+        delta = datetime.now() - self.start_time_stamp
+        s = delta.total_seconds()
+        return s
+
+    def step(self, engine):
+        pass
+
+    def finish(self):
+        self.is_done = True
+
+
+class TerraconProgramEngine:
+    def __init__(self, worker):
+        self.should_stop = False
+        self.program = None
+        self.tasks = list()
+        self.root_task = None
+        self.worker = worker
+
+    def load_program(self, module_name, dir_path='.'):
+        module_full_path = dir_path + '/' + module_name
+        module_spec = importlib.util.spec_from_file_location(module_name, module_full_path)
+        if module_spec is None:
+            print('Module {} not found in {}'.format(module_name, dir_path))
+            return False
+        module = importlib.util.module_from_spec(module_spec)
+        if module is None:
+            return False
+        module_spec.loader.exec_module(module)
+        self.program = module
+        self.root_task = self.program.RootTask("root")
+        if not self.root_task:
+            print("No root task found")
+            return False
+        self.tasks.append(self.root_task)
+
+        return True
+
+    def step(self):
+        tmp_tasks = [task for task in self.tasks if not task.is_done]
+        self.tasks = tmp_tasks
+
+        for task in self.tasks:
+            task.step(self)
+
+    def find_task(self, task_name):
+        for task in self.tasks:
+            if task.name == task_name:
+                return task
+
+    def new_task(self, class_name, task_name = ""):
+        name = class_name
+        if task_name:
+            name = task_name
+        if self.find_task(name):
+            return False
+        task = class_name(name)
+        task.root = self.root_task
+        self.tasks.append(task)
+
+    def current_time(self):
+        now = datetime.now()
+        return now.time()
+
+
 class Worker:
     def __init__(self, use_gpio):
         self.use_gpio = use_gpio
@@ -55,7 +131,7 @@ class Worker:
         self.web_server = WebServer(self)
         self.main_light_intensity = 0
         self.water_on = False
-        self.program_engine = progengine.TerraconProgramEngine()
+        self.program_engine = TerraconProgramEngine(self)
         self.script_mode = True
         self.config_file_path = 'config.json'
         self.read_config(self.config_file_path)
